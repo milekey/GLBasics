3ce5c0141c58f657bd4777ec0bc8f58e3f59687f
milekey <hata@mihr.net>
2022-01-19 14:41:39 +0900

    revised comment

--- app/src/main/java/com/scaredeer/glbasics/MainActivity.kt
+++ app/src/main/java/com/scaredeer/glbasics/MainActivity.kt
@@ -8,12 +8,9 @@ import com.scaredeer.glbasics.framework.Game
 import com.scaredeer.glbasics.framework.Renderer
 
 /**
- * Google の Grafika を Canvas 化し、
- * https://github.com/google/grafika/blob/master/app/src/main/java/com/android/grafika/HardwareScalerActivity.java
+ * cf. https://github.com/Apress/beg-android-games-2ed/tree/master/beginning-android-games-2nd-edition/ch07-gl-basics/src/com/badlogic/androidgames
  * Beginning Android Games のフレームワーク構造を取り入れ、
- * https://github.com/Apress/beg-android-games-2ed/tree/master/beginning-android-games-2nd-edition/ch07-gl-basics/src/com/badlogic/androidgames
- * 両者の良い所取りをしつつ、シングルトンオブジェクトを活用したりして、Kotlin 化した。
- * 基本的に framework のクラスは変更しない。
+ * シングルトンオブジェクトを活用したりして、Kotlin 化した。基本的に framework のクラスは変更しない。
  * Screen をカスタムして、ファミコンのカセットのように入れ換えて使うイメージ。
  */
 class MainActivity : AppCompatActivity() {
