5eaaa67fa5420ae10818079c34f910e58762dd01
milekey <hata@mihr.net>
2022-01-19 22:55:17 +0900

    optimize import

--- app/src/main/java/com/scaredeer/glbasics/framework/Renderer.kt
+++ app/src/main/java/com/scaredeer/glbasics/framework/Renderer.kt
@@ -1,6 +1,6 @@
 package com.scaredeer.glbasics.framework
 
-import android.opengl.GLES20
+import android.opengl.GLES20.glViewport
 import android.opengl.GLSurfaceView
 import android.util.Log
 import java.util.concurrent.locks.ReentrantLock
@@ -40,11 +40,13 @@ class Renderer: GLSurfaceView.Renderer {
         結局、Screen#resize メソッドを追加した。
          */
         stateChange.withLock {
+            /*
             if (state == GLGameState.INITIALIZED) {
                 // 開始 Screen については、Zechner 版のような getStartScreen() 方式は採らず、
                 // MainActivity で直接インスタンス化することにしたので、ここでの処理は不要となる。
-                // Game.screen = getStartScreen()
+                Game.screen = getStartScreen()
             }
+             */
             state = GLGameState.RUNNING
 
             Game.screen.resume()
@@ -55,7 +57,7 @@ class Renderer: GLSurfaceView.Renderer {
     override fun onSurfaceChanged(gl: GL10?, width: Int, height: Int) {
         Log.v(TAG, "onSurfaceChanged (width: $width; height: $height)")
 
-        GLES20.glViewport(0, 0, width, height)
+        glViewport(0, 0, width, height)
 
         Game.screen.resize(width, height)
     }
