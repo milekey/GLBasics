72f8f4c51206018384b2a1a93e27099e29652758
milekey <hata@mihr.net>
2022-01-30 00:23:18 +0900

    Refactoring and optimization

--- app/src/main/java/com/scaredeer/glbasics/TexturedTriangleScreen.kt
+++ app/src/main/java/com/scaredeer/glbasics/TexturedTriangleScreen.kt
@@ -20,30 +20,29 @@ class TexturedTriangleScreen(context: Context) : Screen(context) {
     companion object {
         private val TAG = TexturedTriangleScreen::class.simpleName
 
+        private const val POSITION_COMPONENTS: Int = 2 // x, y（※ z は常に 0 なので省略）
+        private const val TEXTURE_COORDINATE_COMPONENTS: Int = 2 // s, t
         private const val BYTES_PER_FLOAT = 4 // Java float is 32-bit = 4-byte
-        private const val POSITION_COMPONENT_COUNT: Int = 2 // x, y（※ z は常に 0 なので省略）
-        private const val TEXTURE_COORDINATES_COMPONENT_COUNT: Int = 2 // s, t
-
         // stride は要するに、次の頂点データセットへスキップするバイト数を表したもの。
         // 一つの頂点データセットは頂点座標 x, y とテクスチャーの s, t の 4 つで構成されているので、
         // 次の頂点を処理する際に、4 つ分のバイト数だけポインターを先に進める必要が生じる。
-        private const val VERTEX_SIZE =
-            (POSITION_COMPONENT_COUNT + TEXTURE_COORDINATES_COMPONENT_COUNT) * BYTES_PER_FLOAT
+        private const val PER_VERTEX_SIZE =
+            (POSITION_COMPONENTS + TEXTURE_COORDINATE_COMPONENTS) * BYTES_PER_FLOAT
 
         private const val VERTICES_COUNT: Int = 3 // 描画すべき頂点の個数
 
-        private const val U_VP_MATRIX = "u_VpMatrix"
+        private const val U_PROJECTION_MATRIX = "u_ProjectionMatrix"
         private const val A_POSITION = "a_Position"
         private const val A_TEXTURE_COORDINATES = "a_TextureCoordinates"
         private const val V_TEXTURE_COORDINATES = "v_TextureCoordinates"
 
         private const val VERTEX_SHADER = """
-            uniform mat4 $U_VP_MATRIX;
+            uniform mat4 $U_PROJECTION_MATRIX;
             attribute vec4 $A_POSITION;
             attribute vec2 $A_TEXTURE_COORDINATES;
             varying vec2 $V_TEXTURE_COORDINATES;
             void main() {
-                gl_Position = $U_VP_MATRIX * $A_POSITION;
+                gl_Position = $U_PROJECTION_MATRIX * $A_POSITION;
                 $V_TEXTURE_COORDINATES = $A_TEXTURE_COORDINATES;
             }
         """
@@ -111,21 +110,19 @@ class TexturedTriangleScreen(context: Context) : Screen(context) {
         }
     }
 
-    private var shaderProgram: Int = 0
-    private var uVpMatrix: Int = 0
-    private var aPosition: Int = 0
-    private var aTextureCoordinates: Int = 0
-    private var uTextureUnit: Int = 0
+    private var shaderProgram = 0
+    private var uProjectionMatrix = 0
+    private var aPosition = 0
+    private var aTextureCoordinates = 0
+    private var uTextureUnit = 0
 
     private val projectionMatrix = FloatArray(16)
-    private val viewMatrix = FloatArray(16)
-    private val vpMatrix = FloatArray(16)
 
     private val vertices: FloatBuffer
-    private var textureName: Int = 0
+    private var textureName = 0
 
     init {
-        val byteBuffer: ByteBuffer = ByteBuffer.allocateDirect(VERTEX_SIZE * VERTICES_COUNT)
+        val byteBuffer = ByteBuffer.allocateDirect(PER_VERTEX_SIZE * VERTICES_COUNT)
         byteBuffer.order(ByteOrder.nativeOrder())
         vertices = byteBuffer.asFloatBuffer()
     }
@@ -135,7 +132,7 @@ class TexturedTriangleScreen(context: Context) : Screen(context) {
 
         // シェーダーのコンパイル（ここから）------------------------------------------------------------
 
-        val vertexShader: Int = glCreateShader(GL_VERTEX_SHADER)
+        val vertexShader = glCreateShader(GL_VERTEX_SHADER)
         glShaderSource(vertexShader, VERTEX_SHADER)
         glCompileShader(vertexShader)
 
@@ -153,7 +150,7 @@ class TexturedTriangleScreen(context: Context) : Screen(context) {
         glUseProgram(shaderProgram) // シェーダーの選択・有効化
 
         // 各シェーダー変数への入力のポインターとなる id を得る
-        uVpMatrix = glGetUniformLocation(shaderProgram, U_VP_MATRIX)
+        uProjectionMatrix = glGetUniformLocation(shaderProgram, U_PROJECTION_MATRIX)
         aPosition = glGetAttribLocation(shaderProgram, A_POSITION)
         aTextureCoordinates = glGetAttribLocation(shaderProgram, A_TEXTURE_COORDINATES)
         uTextureUnit = glGetUniformLocation(shaderProgram, U_TEXTURE_UNIT)
@@ -180,27 +177,16 @@ class TexturedTriangleScreen(context: Context) : Screen(context) {
     override fun resize(width: Int, height: Int) {
         Log.v(TAG, "resize")
 
-        // 以下、glUniformMatrix4fv の行まで、Projection 行列と View 行列から VP 行列を決定している。
-        // width/height を利用して、ピクセルパーフェクトになるように視界を設定し、
-        // 画面左下が原点になるようにカメラ位置を右上に平行移動している。
+        // width/height を利用してピクセルパーフェクトになるように、
+        // ワールド座標における視界範囲を設定し、Projection 行列を決定している。
         Matrix.orthoM(
             projectionMatrix, 0,
-            -width / 2f, width / 2f,
-            -height / 2f, height / 2f,
-            1f, 3f
+            0f, width.toFloat(),
+            0f, height.toFloat(),
+            1f, -1f
         )
-        Matrix.setLookAtM(
-            viewMatrix, 0,
-            width / 2f, height / 2f, 2f,
-            width / 2f, height / 2f, -1f,
-            0f, 1f, 0f
-        )
-        Matrix.multiplyMM(
-            vpMatrix, 0,
-            projectionMatrix, 0, viewMatrix, 0
-        )
-        // 作成した VP 行列をシェーダー変数（u_VpMatrix）に適用する
-        glUniformMatrix4fv(uVpMatrix, 1, false, vpMatrix, 0)
+        // 早速、作成した Projection 行列をシェーダー変数（u_ProjectionMatrix）に適用する
+        glUniformMatrix4fv(uProjectionMatrix, 1, false, projectionMatrix, 0)
 
         // width/height に基いて、画面下端を底辺とする二等辺三角形の 3 頂点を決定する
         vertices.clear()
@@ -232,10 +218,10 @@ class TexturedTriangleScreen(context: Context) : Screen(context) {
         // 頂点の位置情報の格納場所と書式を指定
         glVertexAttribPointer(
             aPosition,
-            POSITION_COMPONENT_COUNT,
+            POSITION_COMPONENTS,
             GL_FLOAT,
             false,
-            VERTEX_SIZE,
+            PER_VERTEX_SIZE,
             vertices
         )
         // 頂点の位置情報をシェーダー変数 aPosition に対応付ける
@@ -245,14 +231,14 @@ class TexturedTriangleScreen(context: Context) : Screen(context) {
         glBindTexture(GL_TEXTURE_2D, textureName)
 
         // 頂点のテクスチャー位置情報を格納する FloatBuffer のポインターを適切な場所に移動し
-        vertices.position(POSITION_COMPONENT_COUNT)
+        vertices.position(POSITION_COMPONENTS)
         // 頂点情報の格納場所と書式を指定
         glVertexAttribPointer(
             aTextureCoordinates,
-            TEXTURE_COORDINATES_COMPONENT_COUNT,
+            TEXTURE_COORDINATE_COMPONENTS,
             GL_FLOAT,
             false,
-            VERTEX_SIZE,
+            PER_VERTEX_SIZE,
             vertices
         )
         // 頂点のテクスチャー位置情報をシェーダー変数 aTextureCoordinates に対応付ける
