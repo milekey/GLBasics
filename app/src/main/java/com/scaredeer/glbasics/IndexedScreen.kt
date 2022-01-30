c478e9ca20daa53a564874ff1386b1b56aba0e34
milekey <hata@mihr.net>
2022-01-30 15:30:38 +0900

    Refactoring

--- app/src/main/java/com/scaredeer/glbasics/IndexedScreen.kt
+++ app/src/main/java/com/scaredeer/glbasics/IndexedScreen.kt
@@ -4,20 +4,21 @@ import android.content.Context
 import android.opengl.GLES20.*
 import android.opengl.Matrix
 import android.util.Log
-import com.scaredeer.glbasics.framework.gl.Texture.Companion.loadBitmap
 import com.scaredeer.glbasics.framework.Screen
+import com.scaredeer.glbasics.framework.gl.Shader
 import com.scaredeer.glbasics.framework.gl.Texture
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
 import java.nio.FloatBuffer
+import java.nio.ShortBuffer
 
 /**
- * cf. https://github.com/Apress/beg-android-games-2ed/blob/master/beginning-android-games-2nd-edition/ch07-gl-basics/src/com/badlogic/androidgames/glbasics/TexturedTriangleTest.java
+ * cf. https://github.com/Apress/beg-android-games-2ed/blob/master/beginning-android-games-2nd-edition/ch07-gl-basics/src/com/badlogic/androidgames/glbasics/BlendingTest.java
  */
-class TexturedTriangleScreen2(context: Context) : Screen(context) {
+class IndexedScreen(context: Context) : Screen(context) {
 
     companion object {
-        private val TAG = TexturedTriangleScreen2::class.simpleName
+        private val TAG = IndexedScreen::class.simpleName
 
         private const val POSITION_COMPONENTS: Int = 2 // x, y（※ z は常に 0 なので省略）
         private const val TEXTURE_COORDINATE_COMPONENTS: Int = 2 // s, t
@@ -28,81 +29,42 @@ class TexturedTriangleScreen2(context: Context) : Screen(context) {
         private const val PER_VERTEX_SIZE =
             (POSITION_COMPONENTS + TEXTURE_COORDINATE_COMPONENTS) * BYTES_PER_FLOAT
 
-        private const val VERTICES_COUNT: Int = 3 // 描画すべき頂点の個数
+        private const val VERTICES_COUNT: Int = 4 // 頂点の個数
 
-        private const val U_PROJECTION_MATRIX = "u_ProjectionMatrix"
-        private const val A_POSITION = "a_Position"
-        private const val A_TEXTURE_COORDINATES = "a_TextureCoordinates"
-        private const val V_TEXTURE_COORDINATES = "v_TextureCoordinates"
+        private const val BYTES_PER_SHORT = 2 // Java short is 16-bit = 2-byte
+        private const val INDICES_COUNT: Int = 6 // 頂点インデックスの個数
 
-        private const val VERTEX_SHADER = """
-            uniform mat4 $U_PROJECTION_MATRIX;
-            attribute vec4 $A_POSITION;
-            attribute vec2 $A_TEXTURE_COORDINATES;
-            varying vec2 $V_TEXTURE_COORDINATES;
-            void main() {
-                gl_Position = $U_PROJECTION_MATRIX * $A_POSITION;
-                $V_TEXTURE_COORDINATES = $A_TEXTURE_COORDINATES;
-            }
-        """
-
-        private const val U_TEXTURE_UNIT = "u_TextureUnit"
-
-        private const val FRAGMENT_SHADER = """
-            precision mediump float;
-            uniform sampler2D $U_TEXTURE_UNIT;
-            varying vec2 $V_TEXTURE_COORDINATES;
-            void main() {
-                gl_FragColor = texture2D($U_TEXTURE_UNIT, $V_TEXTURE_COORDINATES);
-            }
-        """
+        private const val SQUARE_SIZE_HALF: Float = 256f
     }
 
-    private var shaderProgram = 0
-    private var uProjectionMatrix = 0
-    private var aPosition = 0
-    private var aTextureCoordinates = 0
-    private var uTextureUnit = 0
-
+    private lateinit var shader: Shader
     private lateinit var texture: Texture
 
     private val projectionMatrix = FloatArray(16)
 
     private val vertices: FloatBuffer
+    private val indices: ShortBuffer
 
     init {
-        val byteBuffer = ByteBuffer.allocateDirect(PER_VERTEX_SIZE * VERTICES_COUNT)
+        var byteBuffer = ByteBuffer.allocateDirect(PER_VERTEX_SIZE * VERTICES_COUNT)
         byteBuffer.order(ByteOrder.nativeOrder())
         vertices = byteBuffer.asFloatBuffer()
+
+        byteBuffer = ByteBuffer.allocateDirect(BYTES_PER_SHORT * INDICES_COUNT)
+        byteBuffer.order(ByteOrder.nativeOrder())
+        indices = byteBuffer.asShortBuffer()
+        indices.put(shortArrayOf(
+            0, 1, 2, // 左上 → 左下 → 右上
+            1, 2, 3  // 左下 → 右上 → 右下
+        ))
+        indices.flip()
     }
 
     override fun resume() {
         Log.v(TAG, "resume")
 
-        // シェーダーのコンパイル（ここから）------------------------------------------------------------
-
-        val vertexShader = glCreateShader(GL_VERTEX_SHADER)
-        glShaderSource(vertexShader, VERTEX_SHADER)
-        glCompileShader(vertexShader)
-
-        val fragmentShader = glCreateShader(GL_FRAGMENT_SHADER)
-        glShaderSource(fragmentShader, FRAGMENT_SHADER)
-        glCompileShader(fragmentShader)
-
-        shaderProgram = glCreateProgram()
-        glAttachShader(shaderProgram, vertexShader)
-        glAttachShader(shaderProgram, fragmentShader)
-        glLinkProgram(shaderProgram)
-
-        // シェーダーのコンパイル（ここまで）------------------------------------------------------------
-
-        glUseProgram(shaderProgram) // シェーダーの選択・有効化
-
-        // 各シェーダー変数への入力のポインターとなる id を得る
-        uProjectionMatrix = glGetUniformLocation(shaderProgram, U_PROJECTION_MATRIX)
-        aPosition = glGetAttribLocation(shaderProgram, A_POSITION)
-        aTextureCoordinates = glGetAttribLocation(shaderProgram, A_TEXTURE_COORDINATES)
-        uTextureUnit = glGetUniformLocation(shaderProgram, U_TEXTURE_UNIT)
+        shader = Shader()
+        shader.use()
 
         // 以下 2 行（コメント除く）、 バーテックスシェーダーからフラグメントシェーダーへの橋渡しに使われる
         // u_TextureUnit のための設定（ただし、現状ではデフォルトと一緒なので、省略しても動く）
@@ -110,11 +72,11 @@ class TexturedTriangleScreen2(context: Context) : Screen(context) {
         glActiveTexture(GL_TEXTURE0)
         // Tell the texture uniform sampler to use this texture in the shader by
         // telling it to read from texture unit 0.
-        glUniform1i(uTextureUnit, 0)
+        glUniform1i(shader.uTextureUnit, 0)
 
         val context = weakContext.get()
         if (context != null) {
-            val bitmap = loadBitmap(context, R.drawable.denzi_xanadu)
+            val bitmap = Texture.loadBitmap(context, R.drawable.denzi_xanadu)
             texture = Texture(bitmap!!)
             bitmap.recycle()
 
@@ -138,16 +100,18 @@ class TexturedTriangleScreen2(context: Context) : Screen(context) {
             0f, height.toFloat(),
             1f, -1f
         )
-        // 早速、作成した Projection 行列をシェーダー変数（u_ProjectionMatrix）に適用する
-        glUniformMatrix4fv(uProjectionMatrix, 1, false, projectionMatrix, 0)
+        // 早速、作成した Projection 行列をシェーダーに適用する
+        shader.setProjectionMatrix(projectionMatrix)
 
-        // width/height に基いて、画面下端を底辺とする二等辺三角形の 3 頂点を決定する
+        // 確定した width/height を使って、画面中心から正方形の半分のサイズを上下左右に配置した頂点座標を
+        // 使うことで、 画面中心にセンタリングした正方形を構成する。
         vertices.clear()
         vertices.put(floatArrayOf(
             // x, y, s, t
-            width / 2f, height.toFloat(), 0.5f, 0f, // 頂角
-            0f, 0f, 0f, 1f,                         // 底角（左）
-            width.toFloat(), 0f, 1f, 1f             // 底角（右）
+            width / 2 - SQUARE_SIZE_HALF, height / 2 + SQUARE_SIZE_HALF, 0f, 0f, // 左上
+            width / 2 - SQUARE_SIZE_HALF, height / 2 - SQUARE_SIZE_HALF, 0f, 1f, // 左下
+            width / 2 + SQUARE_SIZE_HALF, height / 2 + SQUARE_SIZE_HALF, 1f, 0f, // 右上
+            width / 2 + SQUARE_SIZE_HALF, height / 2 - SQUARE_SIZE_HALF, 1f, 1f  // 右下
         ))
         vertices.flip()
     }
@@ -172,7 +136,7 @@ class TexturedTriangleScreen2(context: Context) : Screen(context) {
         vertices.position(0)
         // 頂点の位置情報の格納場所と書式を指定
         glVertexAttribPointer(
-            aPosition,
+            shader.aPosition,
             POSITION_COMPONENTS,
             GL_FLOAT,
             false,
@@ -180,13 +144,13 @@ class TexturedTriangleScreen2(context: Context) : Screen(context) {
             vertices
         )
         // 頂点の位置情報をシェーダー変数 aPosition に対応付ける
-        glEnableVertexAttribArray(aPosition)
+        glEnableVertexAttribArray(shader.aPosition)
 
         // 頂点のテクスチャー位置情報を格納する FloatBuffer のポインターを適切な場所に移動し
         vertices.position(POSITION_COMPONENTS)
-        // 頂点情報の格納場所と書式を指定
+        // 頂点のテクスチャー位置情報の格納場所と書式を指定
         glVertexAttribPointer(
-            aTextureCoordinates,
+            shader.aTextureCoordinates,
             TEXTURE_COORDINATE_COMPONENTS,
             GL_FLOAT,
             false,
@@ -194,9 +158,9 @@ class TexturedTriangleScreen2(context: Context) : Screen(context) {
             vertices
         )
         // 頂点のテクスチャー位置情報をシェーダー変数 aTextureCoordinates に対応付ける
-        glEnableVertexAttribArray(aTextureCoordinates)
+        glEnableVertexAttribArray(shader.aTextureCoordinates)
 
-        // 各頂点を描画
-        glDrawArrays(GL_TRIANGLES, 0, VERTICES_COUNT)
+        // 各頂点を描画モードは TRIANGLES + INDICES の代りに、TRIANGLE_STRIP を使うことにする
+        glDrawElements(GL_TRIANGLE_STRIP, INDICES_COUNT, GL_UNSIGNED_SHORT, indices)
     }
 }
